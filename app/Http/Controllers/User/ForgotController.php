29f55a66404f654a88b0f768f7879536515ef44f
Vitalii Koshara <86851008+viking-koshara@users.noreply.github.com>
2021-07-21 12:32:31 +0300

    07-20-2021 VK

--- app/Http/Controllers/User/ForgotController.php
+++ app/Http/Controllers/User/ForgotController.php
@@ -28,37 +28,52 @@ class ForgotController extends Controller
         $input =  $request->all();
         if (User::where('email', '=', $request->email)->count() > 0) {
               // user found
-              $admin = User::where('email', '=', $request->email)->firstOrFail();
-              $autopass = str_random(8);
-              $input['password'] = bcrypt($autopass);
-              $admin->update($input);
-              $subject = "Reset Password Request";
-              $msg = "Your New Password is : ".$autopass;
-              if($gs->is_smtp == 1) {
-                  $data = [
-                      'to' => $request->email,
-                      'subject' => $subject,
-                      'body' => $msg,
-                  ];
-        
-                  $mailer = new GeniusMailer();
+            $admin = User::where('email', '=', $request->email)->firstOrFail();
+            $autopass = str_random(8);
+            $input['password'] = bcrypt($autopass);
+            $admin->update($input);
+            $subject = "Reset Password Request";
+
+            $msg = "Your password has been reseted! New password is " . $autopass . " .<br><br>";
+            $msg .= "Thanks for your contact and we love to support you anytime.<br><br>";
+            $msg .= "All at ProjectShelve<br> ";
+            $msg .= "Call/WhatsApp: (+234) 08147801594<br>";
+            $msg .= "E-mail: projectshelve@gmail.com<br>";
+            $msg .= "Website: www.projectshelve.com<br>";
+
+            if($gs->is_smtp == 1) {
+                $data = [
+                    'to' => $request->email,
+                    'subject' => $subject,
+                    'body' => $msg,
+                ];
+    
+                $mailer = new GeniusMailer();
+                
+                $mailer->sendCustomMail($data);  
                   
-                  $mailer->sendCustomMail($data);  
-                  
-              } else {	
-                  
-                  $headers = "MIME-Version: 1.0" . "\r\n";
-                  $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-                  $headers .= "From: ".$gs->from_name."<".$gs->from_email.">";
-                  
-                  mail($request->email,$subject,$msg,$headers);            
-              }
+            } else {	
+                
+                $to = $request->email;
+
+                $headers = "MIME-Version: 1.0" . "\r\n";
+                $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+                $headers .= "From: ProjectShelve <projectshelve@gmail.com>";
+                                  
+                // mail($request->email,$subject,$msg,$headers);            
+                Mail::send(array(), array(), function ($message) use ($msg, $to, $subject, $headers) {
+                    $message->to($to)
+                    ->subject($subject)
+                    ->setBody($msg,'text/html');
+                });
+            }
               
-              return response()->json('Your Password Reseted Successfully. Please Check your email for new Password.');
-      } else {
-      // user not found
-          return response()->json(array('errors' => [ 0 => 'No Account Found With This Email.' ]));    
-      }  
+            return response()->json('Your Password Reseted Successfully. Please Check your email for new Password.');
+
+        } else {
+        // user not found
+            return response()->json(array('errors' => [ 0 => 'No Account Found With This Email.' ]));    
+        }  
     }
 
 }

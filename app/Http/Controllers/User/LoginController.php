5d662f93a52c7f95e48f6a4cdd0389a205d71635
Vitalii Koshara <86851008+viking-koshara@users.noreply.github.com>
2021-07-21 16:37:55 +0300

    07-21-2021 VK

--- app/Http/Controllers/User/LoginController.php
+++ app/Http/Controllers/User/LoginController.php
@@ -18,65 +18,70 @@ class LoginController extends Controller
 
     public function showLoginForm()
     {
-      $this->code_image();
-      return view('user.login');
+		$this->code_image();
+		return view('user.login');
     }
 
     public function login(Request $request)
     {
         //--- Validation Section
         $rules = [
-                  'email'   => 'required|email',
-                  'password' => 'required'
-                ];
+			'email'   => 'required|email',
+			'password' => 'required'
+		];
 
         $validator = Validator::make(Input::all(), $rules);
         
         if ($validator->fails()) {
-          return response()->json(array('errors' => $validator->getMessageBag()->toArray()));
+			return response()->json(array('errors' => $validator->getMessageBag()->toArray()));
         }
         //--- Validation Section Ends
 
-      // Attempt to log the user in
-      if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-        // if successful, then redirect to their intended location
+		// Attempt to log the user in
+		if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+			// if successful, then redirect to their intended location
 
-        // Check If Email is verified or not
-          if(Auth::guard('web')->user()->email_verified == 'No')
-          {
-            Auth::guard('web')->logout();
-            return response()->json(array('errors' => [ 0 => 'Your Email is not Verified!' ]));   
-          }
+			// Check If Email is verified or not
+			if(Auth::guard('web')->user()->email_verified == 'No')
+			{
+				Auth::guard('web')->logout();
+				return response()->json(array('errors' => [ 0 => 'Your Email is not Verified!' ]));   
+			}
 
-          if(Auth::guard('web')->user()->ban == 1)
-          {
-            Auth::guard('web')->logout();
-            return response()->json(array('errors' => [ 0 => 'Your Account Has Been Banned.' ]));   
-          }
+			if(Auth::guard('web')->user()->ban == 1)
+			{
+				Auth::guard('web')->logout();
+				return response()->json(array('errors' => [ 0 => 'Your Account Has Been Banned.' ]));   
+			}
 
-          // Login Via Modal
-          if(!empty($request->modal))
-          {
-             // Login as Vendor
-            if(!empty($request->vendor))
-            {
-              if(Auth::guard('web')->user()->is_vendor == 2)
-              {
-                return response()->json(route('vendor-dashboard'));
-              }
-              else {
-                return response()->json(route('user-package'));
-                }
-            }
-          // Login as User
-          return response()->json(1);          
-          }
-          // Login as User
-          return response()->json(route('user-dashboard'));
-      }
+			// Login Via Modal
+			if(!empty($request->modal))
+			{
+				dd($request->vendor);
+				return;
+				
+				// Login as Vendor
+				if(!empty($request->vendor))
+				{
+					if(Auth::guard('web')->user()->is_vendor == 2) {
 
-      // if unsuccessful, then redirect back to the login with the form data
-          return response()->json(array('errors' => [ 0 => 'Credentials Doesn\'t Match !' ]));     
+						return response()->json(route('vendor-dashboard'));
+
+					} else {
+
+						return response()->json(route('user-package'));
+					}
+				}
+
+				// Login as User
+				return response()->json(1);          
+			}
+			// Login as User
+			return response()->json(route('user-dashboard'));
+		}
+
+      	// if unsuccessful, then redirect back to the login with the form data
+		return response()->json(array('errors' => [ 0 => 'Credentials Doesn\'t Match !' ]));     
     }
 
     public function logout()
--- app/Http/Controllers/User/RegisterController.php
+++ app/Http/Controllers/User/RegisterController.php
@@ -35,68 +35,66 @@ class RegisterController extends Controller
 	        }    		
     	}
 
-
         //--- Validation Section
-
         $rules = [
-		        'email'   => 'required|email|unique:users',
-		        'password' => 'required|confirmed',
-		        'phone'=>'required',
-				'terms_and_condition'=>'required'
+			'email'   => 'required|email|unique:users',
+			'password' => 'required|confirmed',
+			'phone'=>'required',
+			'terms_and_condition'=>'required'
         ];
         $validator = Validator::make(Input::all(), $rules);
         
         if ($validator->fails()) {
-          return response()->json(array('errors' => $validator->getMessageBag()->toArray()));
-        }
-        //--- Validation Section Ends
+			return response()->json(array('errors' => $validator->getMessageBag()->toArray()));
+        }        
+		//--- Validation Section Ends
 
-	        $user = new User;
-	        $input = $request->all();        
-	        $input['password'] = bcrypt($request['password']);
-	        $token = md5(time().$request->name.$request->email);
-	        $input['verification_link'] = $token;
-	        $input['affilate_code'] = md5($request->name.$request->email);
+		$user = new User;
+		$input = $request->all();        
+		$input['password'] = bcrypt($request['password']);
+		$token = md5(time().$request->name.$request->email);
+		$input['verification_link'] = $token;
+		$input['affilate_code'] = md5($request->name.$request->email);
 
-	          if(!empty($request->vendor))
-	          {
-					//--- Validation Section
-					$rules = [
-						'shop_name' => 'unique:users',
-						'shop_number'  => 'max:10'
-							];
-					$customs = [
-						'shop_name.unique' => 'This Shop Name has already been taken.',
-						'shop_number.max'  => 'Shop Number Must Be Less Then 10 Digit.'
+		if(!empty($request->vendor))
+		{
+			//--- Validation Section
+			$rules = [
+				'shop_name' => 'unique:users',
+				'shop_number'  => 'max:10'
 					];
+			$customs = [
+				'shop_name.unique' => 'This Shop Name has already been taken.',
+				'shop_number.max'  => 'Shop Number Must Be Less Then 10 Digit.'
+			];
 
-					$validator = Validator::make(Input::all(), $rules, $customs);
-					if ($validator->fails()) {
-					return response()->json(array('errors' => $validator->getMessageBag()->toArray()));
-					}
-					$input['is_vendor'] = 1;
+			$validator = Validator::make(Input::all(), $rules, $customs);
+			if ($validator->fails()) {
+				return response()->json(array('errors' => $validator->getMessageBag()->toArray()));
+			}
 
-			  }
+			$input['is_vendor'] = 1;
+		}
 			  
-			$user->fill($input)->save();
-	        if($gs->is_verification_email == 1)
-	        {
+		$user->fill($input)->save();
+		
+		if($gs->is_verification_email == 1)
+		{
 	        $to = $request->email;
 	        $subject = 'Verify your email address.';
 	        $msg = "Dear Customer,<br> We noticed that you need to verify your email address. <a href=".url('user/register/verify/'.$token).">Simply click here to verify. </a>";
 	        //Sending Email To Customer
 	        if($gs->is_smtp == 1)
 	        {
-	        $data = [
-	            'to' => $to,
-	            'subject' => $subject,
-	            'body' => $msg,
-	        ];
-	        $mailer = new GeniusMailer();
-	        $mailer->sendCustomMail($data);
-	        }
-	        else
-	        {
+				$data = [
+					'to' => $to,
+					'subject' => $subject,
+					'body' => $msg,
+				];
+				$mailer = new GeniusMailer();
+				$mailer->sendCustomMail($data);
+
+	        } else {
 				$headers = "MIME-Version: 1.0" . "\r\n";
 				$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 				$headers .= "From: ".$gs->from_name."<".$gs->from_email.">";
@@ -108,47 +106,49 @@ class RegisterController extends Controller
 					->setBody($msg,'text/html');
 				});  
 	        }
+
           	return response()->json('We need to verify your email address. We have sent an email to '.$to.' to verify your email address. Please click link in that email to continue.');
-	        }
-	        else {
-				$to = $request->email;
-				$pwd = $request['password'];
 
-				$headers = "MIME-Version: 1.0" . "\r\n";
-				$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-				$headers .= "From: ".$gs->from_name."<".$gs->from_email.">";
-				
-				$subject = 'Registered Successfully.';
+		} else {
 
-				$msg = "Dear Customer,<br><br> You have successfully registered to ProjectShelve.com.";
-				$msg .= "We wish you have a wonderful experience using our service. Below is your login details. ";
-				$msg .= "Below is your login details. <br>";
-				$msg .= "Email: " . $to . "<br>";
-				$msg .= "Password: " . $pwd . "<br><br>";
-				$msg .= "Thank you.<br><br>";
-								
-				$msg .= "All at ProjectShelve<br> ";
-				$msg .= "Call/WhatsApp: (+234) 08147801594<br>";
-				$msg .= "E-mail: projectshelve@gmail.com<br>";
-				$msg .= "Website: www.projectshelve.com<br>";
+			$to = $request->email;
+			$pwd = $request['password'];
 
-				$sent =   Mail::send(array(), array(), function ($message) use ($msg,$to,$subject,$headers) {
-								$message->to($to)
-								->subject($subject)
-								->setBody($msg,'text/html');
-								});
+			$headers = "MIME-Version: 1.0" . "\r\n";
+			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+			$headers .= "From: ".$gs->from_name."<".$gs->from_email.">";
+			
+			$subject = 'Registered Successfully.';
 
-				$user->email_verified = 'Yes';
-				$user->update();
+			$msg = "Dear Customer,<br><br> You have successfully registered to ProjectShelve.com.";
+			$msg .= "We wish you have a wonderful experience using our service. Below is your login details. ";
+			$msg .= "Below is your login details. <br>";
+			$msg .= "Email: " . $to . "<br>";
+			$msg .= "Password: " . $pwd . "<br><br>";
+			$msg .= "Thank you.<br><br>";
 
-				$notification = new Notification;
-				$notification->user_id = $user->id;
-				$notification->save();
+			$msg .= "All at ProjectShelve<br> ";
+			$msg .= "Call/WhatsApp: (+234) 08147801594<br>";
+			$msg .= "E-mail: projectshelve@gmail.com<br>";
+			$msg .= "Website: www.projectshelve.com<br>";
 
-				Auth::guard('web')->login($user); 
+			$sent =   Mail::send(array(), array(), function ($message) use ($msg,$to,$subject,$headers) {
+							$message->to($to)
+							->subject($subject)
+							->setBody($msg,'text/html');
+							});
 
-				return response()->json(1);
-	        }
+			$user->email_verified = 'Yes';
+			$user->update();
+
+			$notification = new Notification;
+			$notification->user_id = $user->id;
+			$notification->save();
+
+			Auth::guard('web')->login($user); 
+
+			return response()->json(1);
+		}
 
     }
 
@@ -156,23 +156,25 @@ class RegisterController extends Controller
     {
         $gs = Generalsetting::findOrFail(1);
 
-        if($gs->is_verification_email == 1)
-	        {    	
-        $user = User::where('verification_link','=',$token)->first();
-        if(isset($user))
-        {
-            $user->email_verified = 'Yes';
-            $user->update();
-	        $notification = new Notification;
-	        $notification->user_id = $user->id;
-	        $notification->save();
-            Auth::guard('web')->login($user); 
-            return redirect()->route('user-dashboard')->with('success','Email Verified Successfully');
-        }
-    		}
-    		else {
-    		return redirect()->back();	
-    		}
+        if($gs->is_verification_email == 1) {    	
+
+			$user = User::where('verification_link','=',$token)->first();
+
+			if(isset($user))
+			{
+				$user->email_verified = 'Yes';
+				$user->update();
+				$notification = new Notification;
+				$notification->user_id = $user->id;
+				$notification->save();
+				Auth::guard('web')->login($user); 
+				return redirect()->route('user-dashboard')->with('success','Email Verified Successfully');
+			}
+
+		} else {
+
+			return redirect()->back();	
+		}
     }
     
     // Capcha Code Image
@@ -198,17 +200,21 @@ class RegisterController extends Controller
         //$text_color = imagecolorallocate($image, 8, 186, 239);
         $text_color = imagecolorallocate($image, 0, 0, 0);
         $cap_length=6;// No. of character in image
+
         for ($i = 0; $i< $cap_length;$i++)
         {
             $letter = $allowed_letters[rand(0, $length-1)];
             imagettftext($image, 25, 1, 35+($i*25), 35, $text_color, $font, $letter);
             $word.=$letter;
         }
+
         $pixels = imagecolorallocate($image, 8, 186, 239);
+
         for($i=0;$i<500;$i++)
         {
             imagesetpixel($image,rand()%200,rand()%50,$pixels);
         }
+
         session(['captcha_string' => $word]);
         
         imagepng($image, public_path("assets/images/capcha_code.png"));

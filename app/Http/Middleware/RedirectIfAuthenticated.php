5c5f5f0e3907f4ee11756fe5d50e0178b9cd5124
Vitalii Koshara <86851008+viking-koshara@users.noreply.github.com>
2021-07-22 17:38:14 +0300

    07-22-2021 VK

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,19 +17,19 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-      // switch ($guard) {
-      //   case 'admin':
-      //     if (Auth::guard($guard)->check()) {
-      //       return redirect()->route('admin.dashboard');
-      //     }
-      //     break;
+      switch ($guard) {
+        case 'admin':
+          if (Auth::guard($guard)->check()) {
+            return redirect()->route('admin.dashboard');
+          }
+          break;
 
       //   default:
       //     if (Auth::guard($guard)->check()) {
       //         return redirect()->route('user-dashboard');
       //     }
       //     break;
-      // }
+      }
 
         return $next($request);
     }

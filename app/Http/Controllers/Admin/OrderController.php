6a00476457bd3a518465ae035e7e8db28d8ee435
Vitalii Koshara <86851008+viking-koshara@users.noreply.github.com>
2021-07-31 17:44:56 +0300

    07-31-2021

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-require __DIR__ . '/twilio/src/Twilio/autoload.php';
+require '/twilio/src/Twilio/autoload.php';
 
 use App\Classes\GeniusMailer;
 use App\Http\Controllers\Controller;

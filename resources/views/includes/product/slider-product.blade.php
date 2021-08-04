f3f3ba5adadf054eae164382e1b7b261e545bab5
Vitalii Koshara <86851008+viking-koshara@users.noreply.github.com>
2021-08-04 10:55:33 +0300

    08-04-2021

--- resources/views/includes/product/slider-product.blade.php
+++ resources/views/includes/product/slider-product.blade.php
@@ -1,26 +1,26 @@
-<a href="{{ route('front.product', $prod->slug) }}" class="item">
+<a href="{{ route('front.product', $p->slug) }}" class="item">
 		<div class="item-img">
-			@if(!empty($prod->features))
+			@if(!empty($p->features))
 			<div class="sell-area">
-				@foreach($prod->features as $key => $data1)
+				@foreach($p->features as $key => $data1)
 				<span class="sale"
-					style="background-color:{{ $prod->colors[$key] }}">{{ $prod->features[$key] }}</span>
+					style="background-color:{{ $p->colors[$key] }}">{{ $p->features[$key] }}</span>
 				@endforeach
 			</div>
 			@endif
 			<img class="img-fluid"
-				src="{{ $prod->photo ? asset('assets/images/thumbnails/'.$prod->thumbnail):asset('assets/images/noimage.png') }}"
+				src="{{ $p->photo ? asset('assets/images/thumbnails/'.$p->thumbnail):asset('assets/images/noimage.png') }}"
 				alt="Trending Item">
 		</div>
 		<div class="info">
-			<h5 class="name">{{ $prod->showName() }}</h5>
-			<h4 class="price">{{ $prod->showPrice() }}
-				<del><small>{{ $prod->showPreviousPrice() }}</small></del></h4>
+			<h5 class="name">{{ $p->showName() }}</h5>
+			<h4 class="price">{{ $p->showPrice() }}
+				<del><small>{{ $p->showPreviousPrice() }}</small></del></h4>
 			<div class="stars">
 				<div class="ratings">
 					<div class="empty-stars"></div>
 					<div class="full-stars"
-						style="width:{{App\Models\Rating::ratings($prod->id)}}%"></div>
+						style="width:{{App\Models\Rating::ratings($p->id)}}%"></div>
 				</div>
 			</div>
 			<div class="item-cart-area">
@@ -30,7 +30,7 @@
 							@if(Auth::guard('web')->check())
 
 							<span href="javascript:;" class="add-to-wish"
-								data-href="{{ route('user-wishlist-add',$prod->id) }}" data-toggle="tooltip"
+								data-href="{{ route('user-wishlist-add',$p->id) }}" data-toggle="tooltip"
 								data-placement="top" title="{{ $langg->lang54 }}"><i
 									class="icofont-heart-alt"></i>
 							</span>
@@ -46,13 +46,13 @@
 							@endif
 					</li>
 					<li>
-						<span  class="quick-view" rel-toggle="tooltip" title="{{ $langg->lang55 }}" href="javascript:;" data-href="{{ route('product.quick',$prod->id) }}" data-toggle="modal" data-target="#quickview" data-placement="top">
+						<span  class="quick-view" rel-toggle="tooltip" title="{{ $langg->lang55 }}" href="javascript:;" data-href="{{ route('product.quick',$p->id) }}" data-toggle="modal" data-target="#quickview" data-placement="top">
 								<i class="fas fa-shopping-basket"></i>
 						</span>
 					</li>
 					<li>
 							<span href="javascript:;" class="add-to-compare"
-							data-href="{{ route('product.compare.add',$prod->id) }}" data-toggle="tooltip"
+							data-href="{{ route('product.compare.add',$p->id) }}" data-toggle="tooltip"
 							data-placement="top" title="{{ $langg->lang57 }}" >
 							<i class="icofont-exchange"></i>
 						</span>

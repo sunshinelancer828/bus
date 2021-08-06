a554684bfcda6e002af123a93507c00c32e2846c
Vitalii Koshara <86851008+viking-koshara@users.noreply.github.com>
2021-08-06 12:45:10 +0300

    Revert "08-06-2021"
    This reverts commit 4eda4b94493aa59173309d9f5af3d8df50443643.

--- resources/views/layouts/load.blade.php
+++ resources/views/layouts/load.blade.php
@@ -3,7 +3,7 @@
 @yield('content')
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script src="{{asset('assets/admin/js/jqueryui.min.js')}}"></script>
 <script src="{{asset('assets/admin/js/vendors/vue.js')}}"></script>
 <script src="{{asset('assets/admin/js/bootstrap-colorpicker.min.js') }}"></script>
 <script src="{{asset('assets/admin/js/plugin.js')}}"></script>

fe9bb9d050a05ffce7fd2aa9eeb7b920d71264ef
Vitalii Koshara <86851008+viking-koshara@users.noreply.github.com>
2021-08-11 04:52:27 +0300

    08-11-2021

--- public/assets/admin/js/load.js
+++ public/assets/admin/js/load.js
@@ -30,6 +30,27 @@
         }
     }
 // IMAGE UPLOADING PRODUCT :)
+  $(document).on('click', 'input[type="hidden"]#image-upload', () => {
+    const pickerOpts = {
+      types: [
+        {
+          description: 'Images',
+          accept: { 'image/*': ['.png', '.gif', '.jpeg', '.jpg'] }
+        },
+      ],
+      excludeAcceptAllOption: true,
+      multiple: false
+    };
+    
+    async function getTheFile() {
+      // open file picker
+      [fileHandle] = await window.showOpenFilePicker(pickerOpts);
+    
+      // get file contents
+      const fileData = await fileHandle.getFile();
+    };
+  });
+
         $(".img-upload-p").on( "change", function() {
           var imgpath = $(this).parent();
           readURLp(this,imgpath);

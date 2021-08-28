b450ce03a148dc9bb461722bed0f8989de1e039e
Projectshelve <85187226+Projectshelve@users.noreply.github.com>
2021-08-28 08:39:27 -0700

    Update ServiceController.php

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -58,7 +58,7 @@ class ServiceController extends Controller
     {
         //--- Validation Section
         $rules = [
-               'photo'      => 'required|mimes:jpeg,jpg,png,svg',
+               'photo'      => 'required|mimes:jpeg,jpg,png,webp,svg',
                 ];
 
         $validator = Validator::make(Input::all(), $rules);
@@ -98,7 +98,7 @@ class ServiceController extends Controller
     {
         //--- Validation Section
         $rules = [
-               'photo'      => 'mimes:jpeg,jpg,png,svg',
+               'photo'      => 'mimes:jpeg,jpg,png,webp,svg',
                 ];
 
         $validator = Validator::make(Input::all(), $rules);

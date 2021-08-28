2a81bf00cdb6d65e1a222f6bcfea31c6ebc31c79
Projectshelve <85187226+Projectshelve@users.noreply.github.com>
2021-08-28 08:34:46 -0700

    Update SliderController.php

--- app/Http/Controllers/Admin/SliderController.php
+++ app/Http/Controllers/Admin/SliderController.php
@@ -54,7 +54,7 @@ class SliderController extends Controller
     {
         //--- Validation Section
         $rules = [
-               'photo'      => 'required|mimes:jpeg,jpg,png,svg',
+               'photo'      => 'required|mimes:jpeg,jpg,png,webp,svg',
                 ];
 
         $validator = Validator::make(Input::all(), $rules);
@@ -94,7 +94,7 @@ class SliderController extends Controller
     {
         //--- Validation Section
         $rules = [
-               'photo'      => 'mimes:jpeg,jpg,png,svg',
+               'photo'      => 'mimes:jpeg,jpg,png,webp,svg',
                 ];
 
         $validator = Validator::make(Input::all(), $rules);

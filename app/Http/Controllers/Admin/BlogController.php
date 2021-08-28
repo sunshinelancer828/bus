fbf8c1d2dfd165fb4d0c94c8271099f0709389dd
Projectshelve <85187226+Projectshelve@users.noreply.github.com>
2021-08-28 08:44:39 -0700

    Update BlogController.php

--- app/Http/Controllers/Admin/BlogController.php
+++ app/Http/Controllers/Admin/BlogController.php
@@ -52,7 +52,7 @@ class BlogController extends Controller
     {
         //--- Validation Section
         $rules = [
-               'photo'      => 'required|mimes:jpeg,jpg,png,svg',
+               'photo'      => 'required|mimes:jpeg,jpg,png,webp,svg',
                 ];
 
         $validator = Validator::make(Input::all(), $rules);
@@ -106,7 +106,7 @@ class BlogController extends Controller
     {
         //--- Validation Section
         $rules = [
-               'photo'      => 'mimes:jpeg,jpg,png,svg',
+               'photo'      => 'mimes:jpeg,jpg,png,webp,svg',
                 ];
 
         $validator = Validator::make(Input::all(), $rules);

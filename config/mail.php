c783cc67ec287a62e5ea0fa9973568ddf7d75107
Projectshelve <85187226+Projectshelve@users.noreply.github.com>
2021-10-02 05:08:37 -0700

    Update mail

--- config/mail.php
+++ config/mail.php
@@ -86,7 +86,7 @@ return [
 
     'username' => env('MAIL_USERNAME','projectshelve@gmail.com'),
 
-    'password' => env('MAIL_PASSWORD','gbxepuhanjtjjcrp'),
+    'password' => env('MAIL_PASSWORD','lbnnfxalciymhkrw'),
 
     /*
     |--------------------------------------------------------------------------

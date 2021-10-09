e54dab15ec2ff80105a47ec592bd8e3505f5d3fc
webdev <cell828@outlook.com>
2021-10-09 10:02:30 +0900

    test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 use Illuminate\Support\Facades\Route;
+Route::get('/test', function(){
+  var_dump("test");
+});
 
 // ************************************ ADMIN SECTION **********************************************
 Route::prefix('admin')->group(function() {
